47b4db9840e89d0e4935313157fd8a30a61b93c0
Bhavya kumar <166162581+Bhavya-Creates@users.noreply.github.com>
2024-09-06 21:03:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const contactForm = document.getElementById('contactForm');
+        contactForm.addEventListener('submit', function(event) {
+            alert('Thank you for contacting us, we will get back to you shortly!');
+        });
